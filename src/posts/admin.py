98df020f9abea82292c03ce8bb181ca1ef37d5a5
kaon4558 <sokot909@gmail.com>
2019-08-12 07:19:48 +0900

    14강 15강 추가

--- src/posts/admin.py
+++ src/posts/admin.py
@@ -4,7 +4,14 @@ from django.contrib import admin
 from posts.models import Post
 
 class PostAdmin(admin.ModelAdmin):
+	list_display = ["title", "updated", "timestamp"]
+	list_display_links = ["updated"]
+	list_editable = ["title"]
+	list_filter = ["updated", "timestamp"]
+
+	search_fields = ["title", "content"]
 	class Meta:
 		model = Post
+
 admin.site.register(Post, PostAdmin)
 
